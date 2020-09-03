96b3f68291968aee3ff8617d2fd4d2a5bce94a6c
tiffylou <tiffany6872@gmail.com>
2020-09-03 17:25:43 -0400

    Progress

--- lab01_intro_to_coding/exercises/ex1_data_structures.js
+++ lab01_intro_to_coding/exercises/ex1_data_structures.js
@@ -32,10 +32,9 @@ var years = [
 
 // Task 1
 // Console log the length of each Array
-console.log(/* COMPLETE ME */)
-console.log(/* COMPLETE ME */)
-console.log(/* COMPLETE ME */)
-
+console.log(names.length)
+console.log(URLs.length)
+console.log(years.length)
 
 
 // Task 2
@@ -44,9 +43,9 @@ var newName = "The International Center of Photography"
 var newURL = "icp.org"
 var newYear = 1974
 
-names.push(/* COMPLETE ME */);
-URLs[/* COMPLETE ME */] = newURL;
-years = years.concat(/* COMPLETE ME */);
+names.push(newName);
+URLs[URLs.length] = newURL;
+years = years.concat(newYear);
 
 // Task 3
 // construct an Object out of our three Arrays
@@ -60,9 +59,9 @@ var result = {
 
 var museums = {};
 for (var i = 0; i < names.length; i++) {
-  var currentName = names[/* COMPLETE ME */];
-  /* COMPLETE ME */ = URLs[i];
-  var currentYear = /* COMPLETE ME */;
+  var currentName = names[i];
+  var currentURL = URLs[i];
+  var currentYear = years[i];
 
   museums[currentName] = {};
   museums[currentName]["URL"] = currentURL;
@@ -72,7 +71,7 @@ for (var i = 0; i < names.length; i++) {
 console.log('museums', museums)
 
 var museums2 = {};
-names.forEach(function(/* COMPLETE ME*/) {
+names.forEach(function (n, i) {
   museums2[n] = {};
 
   var currentURL = URLs[i];
@@ -84,14 +83,10 @@ names.forEach(function(/* COMPLETE ME*/) {
 
 console.log('museums2', museums2)
 
-// Task
+// Task 4
 // Write a function to add a new museum object, with properties URL and year, to an existing museums object. Call it on museums2
-function addAMuseum(museums, newName, newURL, newYear){
-  /* COMPLETE ME */
+var addAMuseum = (museums, newName, newURL, newYear) => museums[newName] = { URL: newURL, year: newYear };
 
-  return museums;
-}
-
-addAMuseum(/* COMPLETE ME */);
+addAMuseum(museums2, 'The Poe Museum', 'https://www.poemuseum.org/', 1906);
 
 console.log('museums2', museums2);
--- lab01_intro_to_coding/exercises/ex2_quirks_tricks.js
+++ lab01_intro_to_coding/exercises/ex2_quirks_tricks.js
@@ -6,7 +6,7 @@
 var schoolName = "Parsons";
 var schoolYear = 1936;
 
-// Task
+// Task 1
 // What is the value of test3?
 var test1;
 if (1 == true) {
@@ -22,25 +22,27 @@ if (1 === true) {
   test2 = false;
 }
 
-var test3 = test1 === test2;
+var test3 = test1 === test2; // false bc explicit === provides differing results
 
-// Task
-// Change this code so test4 is false and test5 is true. Use console.log() to confirm your cod works.
+// Task 2
+// Change this code so test4 is false and test5 is true. Use console.log() to confirm your code works.
 
-var test4 = 0 == "";
-var test5 = 1 === "1";
+var test4 = 0 === ""; 
+var test5 = 1 == "1";
 
 console.log("test4 is", test4, "and test 5 is", test5);
 
-// Task
+// Task 3
 // What are the values of p, q, and r? Research what is going on here.
 var w = 0.1;
 var x = 0.2;
 var y = 0.4;
 var z = 0.5;
 
-var p = w + x;
+var p = w + x; // 0.30000000000000004
 
-var q = z - x;
+var q = z - x; // 0.3
 
-var r = y - w;
+var r = y - w; // 0.30000000000000004
+
+// This is floating point. It can be rounded to a whole number. 0.1 is often the cause of this issue.
