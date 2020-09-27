2248fbf268cf5017f9c71870d0af3e2e38f20a8a
tiffylou <tiffany6872@gmail.com>
2020-09-27 15:11:52 -0400

    simulation the circle position forever

--- proj-1/index.js
+++ proj-1/index.js
@@ -9,7 +9,12 @@ const tooltip = d3.select('body').append('div')
   .attr('class', 'svg-tooltip');
 
 let data;
-let x;
+
+let xRange = [margin.left, width - margin.right];
+let x = d3.scaleLinear()
+  .domain([0, 1])
+  .range(xRange);
+let centerY = (height - margin.bottom + margin.top) / 2;
 let xAxis;
 let xAxisG;
 let circles;
@@ -43,15 +48,14 @@ let circles;
     d.menPercent = menCount / menTotal;
 
     d.ratio = d.menPercent / (d.womenPercent + d.menPercent);
+
+    d.x = x(d.ratio);
+    d.y = centerY;
   }
 
   topics.sort((a, b) => a.ratio - b.ratio);
 
   data = topics;
-
-  x = d3.scaleLinear()
-    .domain([0, 1])
-    .range([margin.left, width - margin.right]);
 
   xAxis = g => g.call(d3.axisBottom(x).ticks(width / 80));
 
@@ -68,12 +72,6 @@ let circles;
     .attr('transform', `translate(0,${height - margin.bottom})`);
 
   updateGraph();
-
-  svg.append('g')
-    .attr('font-family', 'sans-serif')
-    .attr('font-size', 10)
-    .selectAll('text')
-    .data(data);
 })();
 
 function updateGraph() {
@@ -83,56 +81,50 @@ function updateGraph() {
   xAxisG.transition(t)
     .call(xAxis);
 
-  let simulation = d3.forceSimulation(data)
-    .force('x', d3.forceX(d => x(d.ratio)))
-    .force('y', d3.forceY(d => (height - margin.bottom + margin.top) / 2))
-    .force('collide', d3.forceCollide()
-      .radius(d => 4 + 1))
-    .stop();
-
-  for (let i = 0; i < 50; ++i) simulation.tick();
-
-  const yRandom = d3.randomUniform(height - margin.bottom, margin.top);
-
-  circles.selectAll('circle')
+  let node = circles.selectAll('circle')
     .data(data, d => d.id)
-    .join(
-      enter => enter.append('circle')
-        .attr('stroke', 'none')
-        .attr('fill', 'black')
-        .attr('cx', d => d.x)
-        // .attr('cx', d => x(d.ratio))
-        .attr('cy', d => d.y)
-        // .attr('cy', (height - margin.bottom + margin.top) / 2)
-        // .attr('cy', yRandom)
-        .attr('r', 4)
-        .style('opacity', 1)
-        // .on('mouseover', () => tooltip.style('visibility', 'visible'))
-        .on('mousemove', function (e, d) {
-          console.log(d.topic, d.ratio);
+    .enter().append('circle')
+    .attr('stroke', 'none')
+    .attr('fill', 'black')
+    .attr('r', 4)
+    .style('opacity', 1)
+    // .on('mouseover', () => tooltip.style('visibility', 'visible'))
+    .on('mousemove', function (e, d) {
+      console.log(d.topic, d.ratio);
 
-          // let left = null
-          // let right = null;
+      // let left = null
+      // let right = null;
 
-          // if (e.offsetX < width / 2) {
-          //   left = (e.pageX + 10) + 'px';
-          // } else {
-          //   let bodyMarginRight = parseInt(window.getComputedStyle(document.body).marginRight);
-          //   let tooltipPaddingRight = parseInt(window.getComputedStyle(tooltip.node()).paddingRight);
-          //   right = (document.body.clientWidth - e.pageX + bodyMarginRight + tooltipPaddingRight + 10) + 'px';
-          // }
+      // if (e.offsetX < width / 2) {
+      //   left = (e.pageX + 10) + 'px';
+      // } else {
+      //   let bodyMarginRight = parseInt(window.getComputedStyle(document.body).marginRight);
+      //   let tooltipPaddingRight = parseInt(window.getComputedStyle(tooltip.node()).paddingRight);
+      //   right = (document.body.clientWidth - e.pageX + bodyMarginRight + tooltipPaddingRight + 10) + 'px';
+      // }
 
-          // tooltip
-          //   .style('top', (e.pageY - 10) + 'px')
-          //   .style('left', left)
-          //   .style('right', right)
-          //   .html(`Title: ${d.title}<br>Year: ${d.publicationYear}<br>Rating: ${d.avgRating}`);
-        }),
-      // .on('mouseout', () => tooltip.style('visibility', 'hidden')),
-      update => update
-        .call(update => update.transition(t)
-          .attr('cx', d => x(new Date(d.publicationYear)))
-          .attr('cy', d => y(d.avgRating))
-        ),
-    );
+      // tooltip
+      //   .style('top', (e.pageY - 10) + 'px')
+      //   .style('left', left)
+      //   .style('right', right)
+      //   .html(`Title: ${d.title}<br>Year: ${d.publicationYear}<br>Rating: ${d.avgRating}`);
+    })
+  // .on('mouseout', () => tooltip.style('visibility', 'hidden')),
+
+  const radius = 4 + 1;
+
+  d3.forceSimulation(data)
+    .force('x', d3.forceX(d => x(d.ratio)))
+    .force('y', d3.forceY(centerY))
+    .force('collide', d3.forceCollide().radius(d => radius))
+    .on('tick', tickActions);
+
+  function tickActions() {
+    node
+      .attr('cx', d => {
+        d.x = Math.max(xRange[0] + radius, Math.min(xRange[1] - radius, d.x));
+        return d.x;
+      })
+      .attr('cy', d => d.y);
+  }
 }
