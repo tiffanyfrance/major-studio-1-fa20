f3ffe54b651bce094ca094f241efa0216af63fbf
tiffylou <tiffany6872@gmail.com>
2020-12-03 20:03:38 -0500

    fix audio player

--- proj-3/tips.js
+++ proj-3/tips.js
@@ -1,6 +1,8 @@
 // building each song tooltip
 import { data } from './data.js';
 
+let arrWaves = [];
+
 d3.select('#corridos')
   .selectAll('.tip')
   .data(data)
@@ -10,19 +12,19 @@ d3.select('#corridos')
   .attr('id', d => d.id)
   .html(d => {
     return `<h4>${d.trackName}</h4>
-            <a onclick="wavesurfer.playPause()"><img src="play.svg" class="wave-btn"></a>
+            <img src="play.svg" class="wave-btn">
             <div class="waves" id="${d.id}-wf"></div>
             <p>${d.trackStr}</p>`
   })
   .style('top', d => d.top + 'px')
   .style('left', d => d.left + 'px')
   .each(function(d) {
-    buildWave(`#${d.id}-wf`, d.wavecolor, d.progresscolor, d.trackMP3)
+    buildWave(d.id, d.wavecolor, d.progresscolor, d.trackMP3)
   });
 
 function buildWave(id, wavecolor, progresscolor, mp3) {
   let wavesurfer = WaveSurfer.create({
-    container: document.querySelector(id),
+    container: document.querySelector(`#${id}-wf`),
     barWidth: 3,
     barRadius: 3,
     cursorWidth: 0,
@@ -34,6 +36,16 @@ function buildWave(id, wavecolor, progresscolor, mp3) {
   });
 
   wavesurfer.load(mp3);
+
+  arrWaves.push(wavesurfer);
+
+  document.querySelector(`#${id}`).onclick = () => {
+    for (let ws of arrWaves) {
+      if (ws !== wavesurfer) ws.stop();
+    }
+
+    wavesurfer.playPause();
+  };
 }
 
 // d3.select('#corridos-01')
